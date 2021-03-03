d2f083257cd255e2d1855ee2eb4e3e6361d446b0
Brady Peterson <bpeterson1663@gmail.com>
2021-03-03 09:04:58 -0600

    feat(rendering content): learning basics in react native and rendering content

--- first-project/App.tsx
+++ first-project/App.tsx
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [allGoals, setAllGoals] = useState<{key: string, value: string}[]>([])
+
+  const handleInput = (enteredText: string): void => setEnteredGoal(enteredText)
+
+  const handleSubmit = (): void => {
+    setAllGoals(currentGoals => [...currentGoals, {value: enteredGoal, key: Math.random().toString()}])
+    setEnteredGoal('')
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.container}>
+        <TextInput placeholder="Enter Goal" style={styles.input} value={enteredGoal} onChangeText={handleInput}/>
+        <Button onPress={handleSubmit} title="ADD" />
+      </View>
+      <FlatList data={allGoals} renderItem={itemData => <View style={styles.listItem}><Text>{itemData.item.value}</Text></View> } />
+        
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30
   },
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: 200,
+    borderColor: 'black',
+    borderWidth: 1
+  },
+  listItem: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: '#ccc',
+    borderCOlor: 'black',
+    borderWidth: 1
+  }
 });
