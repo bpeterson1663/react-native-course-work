50e9527a3a5882ec7417054e938cceb505042626
Brady Peterson <bpeterson1663@gmail.com>
2021-03-07 04:35:20 -0600

    feat(in play): create in play screen

---
+++ guess-number/screens/PlayScreen.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { View, Text, StyleSheet } from 'react-native'
+
+export const InPlayScreen: React.FC = (): JSX.Element => {
+    return (
+        <View style={styles.screen}>
+            <Text>Guess A Number</Text>    
+        </View>
+    )
+}
+
+InPlayScreen.propTypes = {
+    startGame: PropTypes.func.isRequired
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: 'center'
+    }
+})
