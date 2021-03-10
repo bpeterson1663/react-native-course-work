0183dad6be6be9c88d90c909b991a377ab20a653
Brady Peterson <bpeterson1663@gmail.com>
2021-03-09 21:47:45 -0600

    feat(styling): refactor styles for start game screen

--- guess-number/screens/StartGameScreen.tsx
+++ guess-number/screens/StartGameScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native'
 
 interface StartGameScreenT {
     startGame: () => void
@@ -8,8 +8,16 @@ interface StartGameScreenT {
 export const StartGameScreen: React.FC<StartGameScreenT> = ({ startGame }): JSX.Element => {
     return (
         <View style={styles.screen}>
-            <Text>Game Screen</Text>
-            <Button onPress={startGame} title="Start Game"></Button>
+            <Text style={styles.title}>Start a new Game!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput />
+                <View style={styles.buttonContainer}>
+                    <Button onPress={startGame} title="Reset"></Button>
+                    <Button onPress={startGame} title="Confirm"></Button>
+                </View>
+            </View>
+            
         </View>
     )
 }
@@ -23,5 +31,20 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 10,
         alignItems: 'center'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 })
